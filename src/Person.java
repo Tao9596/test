118cb5eebbf39ed2be99184ca8ce87112883974f
lock <20171275@qq.com>
2016-08-27 02:09:48 +0800

    add src

---
+++ src/Person.java
@@ -0,0 +1,13 @@
+public class Person
+{
+	private String name;
+	private String email;
+	private int age;
+
+	public Persion(){}
+	public Persion(String name, String email, int age){
+		this.name = name;
+		this.email = email;
+		this.age = age;
+	}
+}
